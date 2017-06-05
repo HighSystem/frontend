efcf397e65d41de9324ed22a072b3fb059dd2358
Luis Fernnado <trossky.dev@gmail.com>
2017-06-05 00:04:27 -0500

    adding modules of AngularMaterial Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
 
+/*
+************************************************
+*    Material modules for app
+*************************************************
+*/
+import { MyMaterialModule } from './app.material';
+/*
+************************************************
+*     principal component
+*************************************************
+*/
 import { AppComponent } from './app.component';
+/*
+************************************************
+*     modules of  your app
+*************************************************
+*/
 
+/*
+************************************************
+*     services of  your app
+*************************************************
+*/
+
+/*
+************************************************
+*     models of  your app
+*************************************************
+*/
+
+/*
+************************************************
+*     constant of  your app
+*************************************************
+*/
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyMaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
